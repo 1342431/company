294a3424b33b5046bd6ed16481ccf113946b0279
1342431 <1342431bcltd@gmail.com>
2022-07-26 18:52:09 -0700

    css

--- src/components/Thanks.vue
+++ src/components/Thanks.vue
@@ -1,20 +1,22 @@
 <script setup>
 </script>
 <template>
-  <div>
+  <div class="thanks_div">
     <h1 class="thanks">Thank you!!</h1>
-    <h3 class="thanks_2">We will reach out to you shortly</h3>
+    <h3 class="thanks_2">We will reach out to you shortly.</h3>
   </div>
 </template>
 <style scoped>
 .thanks{
-  top: 200px;
   text-align: center;
   color: #ffffff;
 }
 .thanks_2{
-  top: 210px;
   text-align: center;
   color: #c18d71;
 }
+.thanks_div {
+padding: 225px 0;
+}
+
 </style>
